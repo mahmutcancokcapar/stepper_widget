9441cc7f4e18b7ee5ea55b024ded064426490bbf
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-08-12 03:22:48 +0300

    Add files via upload

---
+++ lib/stepper_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class StepperClass extends StatefulWidget {
+  const StepperClass({super.key});
+
+  @override
+  State<StepperClass> createState() => _StepperClassState();
+}
+
+class _StepperClassState extends State<StepperClass> {
+  int _currentStep = 0;
+  void onStepTapped(int index) {
+    setState(() {
+      _currentStep = index;
+    });
+  }
+
+  void onStepContinue() {
+    setState(() {
+      if (_currentStep < 3) {
+        _currentStep++;
+      }
+    });
+  }
+
+  void onStepCancel() {
+    setState(() {
+      if (_currentStep > 0) {
+        _currentStep--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stepper(
+          currentStep: _currentStep,
+          onStepTapped: onStepTapped,
+          onStepContinue: onStepContinue,
+          onStepCancel: onStepCancel,
+          steps: [
+            Step(
+              isActive: _currentStep == 0,
+              title: const Text('Başlık'),
+              content: const Text('Açıklama 1'),
+            ),
+            Step(
+              isActive: _currentStep == 1,
+              title: const Text('Step 2'),
+              content: const Text('Açıklama 2'),
+            ),
+            Step(
+              isActive: _currentStep == 2,
+              title: const Text('Step 3'),
+              content: const Text('Açıklama 1'),
+            ),
+            Step(
+              isActive: _currentStep == 3,
+              title: const Text('Step 4'),
+              content: const Text('Açıklama 1'),
+            ),
+          ],
+          controlsBuilder: (context, details) {
+            return Row(
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                if (_currentStep < 3)
+                  ElevatedButton(
+                    onPressed: onStepContinue,
+                    child: const Text('Bir Sonraki'),
+                  )
+                else
+                  const ElevatedButton(
+                    onPressed: null,
+                    child: Text('Tamamlandı'),
+                  ),
+                const SizedBox(
+                  width: 10,
+                ),
+                if (_currentStep > 0)
+                  ElevatedButton(
+                    onPressed: onStepCancel,
+                    child: const Text('Bir Önceki'),
+                  ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
